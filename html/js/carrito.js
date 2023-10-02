35714bc73fd064f343a5dc45dd3b40124ef2f411
jhon-frand <frandlebaza@gmail.com>
2023-10-02 11:36:03 -0500

    mercadolibre

--- html/js/carrito.js
+++ html/js/carrito.js
@@ -4,16 +4,21 @@ let cantProductos = document.getElementById("cantProducts")
 let deleteButton = document.getElementById("delete")
 let btn3 = document.getElementById("btn-3")
 let btn1 = document.getElementById("btn-1")
+let numProduct = document.getElementById("numProduct")
 
 let num = 0;
 let valor = 25300;
 btn3.addEventListener("click", function() {
-    
     num++;
     count.textContent = num;
     subTotal.textContent = valor * num;
     cantProductos.textContent = num;
-    
+
+    if (num == 1) {
+        numProduct.textContent = "producto";
+    } else if (num > 1) {
+        numProduct.textContent = "productos"
+    }
 })
 
 btn1.addEventListener("click", function() {
@@ -22,15 +27,16 @@ btn1.addEventListener("click", function() {
         count.textContent = num;
         subTotal.textContent = valor * num;
         cantProductos.textContent = num;
-    } else if (num <= 0) {
+    } else if (num == 0) {
         count.textContent = 0;
         subTotal.textContent = 0;
     }
 })
+
 deleteButton.addEventListener("click", function() {
     num = 0;
     count.textContent = 0;
     subTotal.textContent = 0;
     cantProductos.textContent = 0;
-   
+    numProduct.textContent = " ";
 })   

a42c0405eb14fba13ce66ffcd329bdeef9b9e00c
jhon-frand <frandlebaza@gmail.com>
2023-10-17 11:40:30 -0500

    shopp

--- html/js/mercadolibre.js
+++ html/js/mercadolibre.js
@@ -1,17 +1,23 @@
 let inputBusqueda = document.getElementById('inputBusqueda');
 
 class Automovil {
-    constructor(marca, modelo){
+    constructor(marca, modelo, precio, año, kms, direccion, imagen){
         this.marca = marca;
         this.modelo = modelo;
+        this.precio = precio;
+        this.año = año;
+        this.kms = kms;
+        this.direccion = direccion;
+        this.imagen = imagen;
     }
 };
 
-let auto1 = new Automovil("JEEP", "RUBICON");
-alert ( auto1.marca + " " + auto1.modelo); 
+let auto1 = new Automovil("Jeep", "Rubicon", 150000000, 2019, 51000, "Pitalito - Huila", "css/img/rubicon.jpg" );
 
-inputBusqueda.addEventListener("keydown", function(event){
-    if (event.key == "Enter"){ 
+// alert ( auto1.marca + " " + auto1.modelo); 
+
+inputBusqueda.addEventListener("click", function(event){
+    // if (event.key == "Enter"){ 
     let mainContent = document.getElementById("mainContent");
     //caja producto
     let boxProducto = document.createElement("div");
@@ -24,8 +30,12 @@ inputBusqueda.addEventListener("keydown", function(event){
 
     let imgAuto = document.createElement("img");
     boxImg.appendChild(imgAuto);
-    imgAuto.setAttribute("src", "css/img/stalk.jpg");
+    imgAuto.setAttribute("src", auto1.imagen);
     imgAuto.setAttribute("class","img-auto");
+
+    let icon = document.createElement("i");
+    boxImg.appendChild(icon);
+    icon.setAttribute("class","icon-heart fa-regular fa-heart");
 
     //caja información
     let boxInfo = document.createElement("div");
@@ -36,6 +46,37 @@ inputBusqueda.addEventListener("keydown", function(event){
     boxInfo.appendChild(marca);
     let txtNodeMarca = document.createTextNode(auto1.marca + " " + auto1.modelo);
     marca.appendChild(txtNodeMarca);
-}
+    marca.setAttribute("class", "marca");
+    ///precio
+    let precio = document.createElement("label");
+    boxInfo.appendChild(precio);
+
+    //línea para convertir el número a formato de precio con . en los miles
+    let precioStr = Intl.NumberFormat("de-DE").format(auto1.precio);
+
+
+    let txtNodePrecio = document.createTextNode("$ "+ precioStr);
+    precio.appendChild(txtNodePrecio);
+    precio.setAttribute("class", "precio");
+
+    let año = document.createElement("label");
+    boxInfo.appendChild(año);
+    let txtNodeAño = document.createTextNode(auto1.año + " ");
+    año.appendChild(txtNodeAño);
+    año.setAttribute("class", "año");
+
+    let kms = document.createElement("label");
+    boxInfo.appendChild(kms);
+    let kmStr = Intl.NumberFormat("de-DE").format(auto1.kms);
+    let txtNodeKm = document.createTextNode(kmStr +" km ");
+    kms.appendChild(txtNodeKm);
+    kms.setAttribute("class", "kms");
+
+    let direccion = document.createElement("label");
+    boxInfo.appendChild(direccion);
+    let txtNodeDir = document.createTextNode(auto1.direccion)
+    direccion.appendChild(txtNodeDir);
+    direccion.setAttribute("class", "direccion");
+// }
 
 });

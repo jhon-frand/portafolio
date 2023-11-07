59022ae34ca5873a53b7c1b9c26b5459dd5c2461
jhon-frand <frandlebaza@gmail.com>
2023-11-07 09:08:50 -0500

    mercado

--- html/js/mercadolibre.js
+++ html/js/mercadolibre.js
@@ -12,16 +12,24 @@ class Automovil {
     }
 };
 
-let auto1 = new Automovil("Jeep", "Rubicon", 150000000, 2019, 51000, "Pitalito - Huila", "css/img/rubicon.jpg" );
-let auto2 = new Automovil("Mazda", "6", 243000000, 2023, 0, "Bogotá DC", "css/img/mazda.jpg" );
-let auto3 = new Automovil("Toyota", "xl", 200000000, 2023, 12000, "Cali", "css/img/toyota2.jpg" );
-let auto4 = new Automovil("Bettle", "Clasic", 134000000, 2023,5000, "Medellín", "css/img/bettle2.jpg" );
+let img1 = "../imagenes/rubicon.jpg";
+let img2 = "../imagenes/mazda.jpg";
+let img3 = "../imagenes/toyota2.jpg";
+let img4 = "../imagenes/bettle2.jpg";
 
+let auto1 = new Automovil("Jeep", "Rubicon", 150000000, 2019, 51000, "Pitalito - Huila", img1 );
+let auto2 = new Automovil("Mazda", "6", 243000000, 2023, 0, "Bogotá DC", img2 );
+let auto3 = new Automovil("Toyota", "xl", 200000000, 2023, 12000, "Cali", img3 );
+let auto4 = new Automovil("Bettle", "Clasic", 134000000, 2023,5000, "Medellín", img4 );
+
+let autos = [auto1, auto2, auto3, auto4];
 
     cargarVehiculo(auto1);
     cargarVehiculo(auto2);
     cargarVehiculo(auto3);
     cargarVehiculo(auto4);
+
+
 
 function cargarVehiculo(auto){
 
@@ -89,6 +97,75 @@ function cargarVehiculo(auto){
 // }
 
 };
-inputBusqueda.addEventListener("click", function(event){
-   
-});
+
+
+inputBusqueda.addEventListener("input", function(event) {
+    let userInput = event.target.value.toLowerCase();
+    mainContent.innerHTML = ''; // Limpia el contenido anterior
+
+    for (let auto of autos) {
+        if (auto.marca.toLowerCase() === userInput) {
+            // Crea la caja de producto para el auto correspondiente
+        
+            let mainContent = document.getElementById("mainContent");
+            //caja producto
+            let boxProducto = document.createElement("div");
+            mainContent.appendChild(boxProducto);
+            boxProducto.setAttribute("class", "box-producto");
+            //caja imagen
+            let boxImg = document.createElement("div");
+            boxProducto.appendChild(boxImg);
+            boxImg.setAttribute("class", "box-img");
+        
+            let imgAuto = document.createElement("img");
+            boxImg.appendChild(imgAuto);
+            imgAuto.setAttribute("src", auto.imagen);
+            imgAuto.setAttribute("class","img-auto");
+        
+            let icon = document.createElement("i");
+            boxImg.appendChild(icon);
+            icon.setAttribute("class","icon-heart fa-regular fa-heart");
+        
+            //caja información
+            let boxInfo = document.createElement("div");
+            boxProducto.appendChild(boxInfo);
+            boxInfo.setAttribute("class", "box-info");
+        
+            let marca = document.createElement("label");
+            boxInfo.appendChild(marca);
+            let txtNodeMarca = document.createTextNode(auto.marca + " " + auto.modelo);
+            marca.appendChild(txtNodeMarca);
+            marca.setAttribute("class", "marca");
+        
+            ///precio
+            let precio = document.createElement("label");
+            boxInfo.appendChild(precio);
+        
+            //línea para convertir el número a formato de precio con . en los miles
+            let precioStr = Intl.NumberFormat("de-DE").format(auto.precio);
+        
+        
+            let txtNodePrecio = document.createTextNode("$ "+ precioStr);
+            precio.appendChild(txtNodePrecio);
+            precio.setAttribute("class", "precio");
+        
+            let año = document.createElement("label");
+            boxInfo.appendChild(año);
+            let txtNodeAño = document.createTextNode(auto.año + " ");
+            año.appendChild(txtNodeAño);
+            año.setAttribute("class", "año");
+        
+            let kms = document.createElement("label");
+            boxInfo.appendChild(kms);
+            let kmStr = Intl.NumberFormat("de-DE").format(auto.kms);
+            let txtNodeKm = document.createTextNode(kmStr +" km ");
+            kms.appendChild(txtNodeKm);
+            kms.setAttribute("class", "kms");
+        
+            let direccion = document.createElement("label");
+            boxInfo.appendChild(direccion);
+            let txtNodeDir = document.createTextNode(auto.direccion)
+            direccion.appendChild(txtNodeDir);
+            direccion.setAttribute("class", "direccion");
+
+        }}});
